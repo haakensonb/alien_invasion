d94f38316fe12ece6f8f321a265ecca19c31e20a
haakensonb <untamedhawk44@gmail.com>
2017-06-26 15:30:56 -0400

    Ship class added and ship drawn at bottom of screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 def run_game():
     # Initialize game and create screen object
     pygame.init()
@@ -11,6 +13,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_height, ai_settings.screen_width))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
 
     # Set the background color
     bg_color = (230, 230, 230)
@@ -25,6 +30,7 @@ def run_game():
 
         # Redraw screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
