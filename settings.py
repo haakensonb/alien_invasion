74140bd701273b9ebdb9a541f6bba3cf337bce53
haakensonb <untamedhawk44@gmail.com>
2017-06-26 16:35:05 -0400

    Ship is able to move right and left within scree

--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ class Settings():
         self.screen_height = 1200
         self.screen_width = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """ Initialize the ship and it's starting position """
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get it's rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,24 @@ class Ship():
         # Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store decimal value for ship's center
+        self.center = float(self.rect.centerx)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """ Update ship's position based on movement flag """
+        # Update ship's center value not rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """ Draw ship at it's current location """
